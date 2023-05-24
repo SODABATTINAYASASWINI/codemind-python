eb166cc3ed9da20e2ea36c96709b566d91a23628
SODABATTINAYASASWINI <126274756+SODABATTINAYASASWINI@users.noreply.github.com>
2023-05-24 14:33:04 +0530

    n=int(input()) a=list(map(int,input().split())) p=[] s=sum(a) l=len(a) avg=s//l for i in a:     if(i>=avg):         p.append(i) print(len(p))

--- Elements_greater_than_average.py
+++ Elements_greater_than_average.py
@@ -1,11 +1,10 @@
 n=int(input())
 a=list(map(int,input().split()))
-l=len(a)
-s=sum(a)
 p=[]
-avg=(s//l)
+s=sum(a)
+l=len(a)
+avg=s//l
 for i in a:
     if(i>=avg):
         p.append(i)
 print(len(p))
-        
