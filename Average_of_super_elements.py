8f7c2463b5adb17e193da6aa0931aca89b5f01f2
SODABATTINAYASASWINI <126274756+SODABATTINAYASASWINI@users.noreply.github.com>
2023-06-10 21:38:12 +0530

    n=int(input()) a=list(map(int,input().split())) v=[] for i in a:     c=a.count(i)     if(c==i and c not in v):         v.append(c) if(len(v)==0):     print("-1") else:     print("%.2f"%(sum(v)/len(v)))

---
+++ Average_of_super_elements.py
@@ -0,0 +1,11 @@
+n=int(input())
+a=list(map(int,input().split()))
+v=[]
+for i in a:
+    c=a.count(i)
+    if(c==i and c not in v):
+        v.append(c)
+if(len(v)==0):
+    print("-1")
+else:
+    print("%.2f"%(sum(v)/len(v)))
