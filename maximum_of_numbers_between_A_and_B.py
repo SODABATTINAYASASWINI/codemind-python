572fcbe45112468b61816edec68c4856d970376a
SODABATTINAYASASWINI <126274756+SODABATTINAYASASWINI@users.noreply.github.com>
2023-06-15 21:54:07 +0530

    n=int(input()) l=list(map(int,input().split())) a,b=map(int,input().split()) v=[] for i in l:     if i>=a and i<=b:         v.append(i) if(len(v)==0):     print("-1") else:     print(max(v))

---
+++ maximum_of_numbers_between_A_and_B.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+a,b=map(int,input().split())
+v=[]
+for i in l:
+    if i>=a and i<=b:
+        v.append(i)
+if(len(v)==0):
+    print("-1")
+else:
+    print(max(v))
